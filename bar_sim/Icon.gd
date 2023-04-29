c530169ecd0dc759127c65e06ea7bfb21e554fa0
Robert Grant <rg11rant@gmail.com>
2023-04-29 13:42:39 -0600

    add bitart

---
+++ bar_sim/Icon.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var fart = 2
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var too = "cat"
+	too = 3
+	print(too)
+	pass
